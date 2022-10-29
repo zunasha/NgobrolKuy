1b9db014fd9be1775375d9cee326cbe7ddd609a3
Shikirashi <65103461+Shikirashi@users.noreply.github.com>
2022-10-29 22:56:43 +0700

    Initial commit

---
+++ app/src/main/java/com/responsi/ngobrolkuy/ProfilePicActivity.java
@@ -0,0 +1,14 @@
+package com.responsi.ngobrolkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilePicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_pic);
+    }
+}
